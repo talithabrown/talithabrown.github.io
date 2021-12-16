84acb264ccf5e71b9d1606808bf72cf8920c0af8
talithabrown <talithabrown128@gmail.com>
2021-12-15 19:38:23 -0700

    adding javascript to read json file for rentals pg

--- scoots-website/js/rentals.js
+++ scoots-website/js/rentals.js
@@ -1,13 +1,63 @@
+// https://github.com/talithabrown/talithabrown.github.io/commit/21d4a651113efe3b5c0747df00f19b400e725c5b
 
-// $.getJSON("./data/rental-info.json", function(json) {
-//     console.log(json); // this will show the info it in firebug console
-// });
+const requestURL = 'https://talithabrown.github.io/data.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        //console.table(jsonObject);  
+        const rentals = jsonObject['rentals'];
+        
+        let tableRow = document.createElement('tr');
+
+        for (let i = 0; i < rentals.length; i++) {
+            let td1 = document.createElement('td');
+            td1.textContent = rentals[i].name;
+            tableRow.appendChild(td1);
+
+            let td2 = document.createElement('td');
+            td2.textContent = rentals[i].reserveHalfDayPrice;
+            tableRow.appendChild(td2);
+
+            let td3 = document.createElement('td');
+            td3.textContent = rentals[i].reserveFullDayPrice;
+            tableRow.appendChild(td3);
+
+            let td4 = document.createElement('td');
+            td4.textContent = rentals[i].walkinHalfDayPrice;
+            tableRow.appendChild(td4);
+
+            let td5 = document.createElement('td');
+            td5.textContent = rentals[i].walkinFullDayPrice;
+            tableRow.appendChild(td5);
+
+            document.querySelector('table.price-table').appendChild(tableRow);
+        };
+
+        // document.querySelector('table.price-table').appendChild(tableRow);
+    })
 
 
-$.getJSON("data.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
 
+    
+    // temporary checking for valid response and data parsing
+    //     const towns = jsonObject['towns'];
+
+    //     let card = document.createElement('section');
+
+    //     for (let i = 0; i < towns[6].events.length; i++) {
+    //         let p = document.createElement('p');
+    //         p.textContent = towns[6].events[i];
+    //         card.appendChild(p);
+    //     };
+
+    //     document.querySelector('div.events').appendChild(card);
+
+    // });
+
+    
     // for (let i = 0; i < towns.length; i++) {
     //     let card = document.createElement('section');
 
